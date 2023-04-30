b8354ad25be092f18f4091d48a4f271fd883c479
Joel <joel02varghese@gmail.com>
2023-04-30 23:58:28 +0530

    Change status field when approved or finished or either toggled

--- backend/examples/views/example_state.py
+++ backend/examples/views/example_state.py
@@ -33,17 +33,27 @@ class ExampleStateList(generics.ListCreateAPIView):
             if Member.objects.has_role(self.kwargs["project_id"], self.request.user, "annotation_approver"):
                 if example.annotations_approved_by is None:
                     example.annotations_approved_by = self.request.user
+                    example.status = example.Status.APPROVED
                 else :
                     example.annotations_approved_by = None
+                    example.status = example.Status.FINISHED
                 example.save()
                 return
             queryset.delete()
+            # Changes status to in progress if finished example state exists
+            example.status = example.Status.IN_PROGRESS
+            example.save()
         else:
             if Member.objects.has_role(self.kwargs["project_id"], self.request.user, "annotation_approver"):
                 if example.annotations_approved_by is None:
                     example.annotations_approved_by = self.request.user
+                    example.status = example.Status.APPROVED
                 else:
                     example.annotations_approved_by = None
+                    example.status = example.Status.IN_PROGRESS
                 example.save()
                 return
+            # Changes status to finished if finished example state doesn't exist
+            example.status = example.Status.FINISHED
+            example.save()
             serializer.save(example=example, confirmed_by=self.request.user)
